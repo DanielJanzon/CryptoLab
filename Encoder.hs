1deb34f3a9a713144eda0182d82a1c7186eafb0f
Daniel Janzon <janzon@gmail.com>
2011-12-21 00:48:24 +0100

    Add function to decode an integer

--- Encoder.hs
+++ Encoder.hs
@@ -1,14 +1,21 @@
 module Encoder
-(
-byte_str_to_odd
+(byte_str_to_odd
+,encode_number
+,decode_number
 ) where
 
 import Data.Word
+--import Data.Char
 
--- Craft an odd arbitrary sized integer from a byte string
-make_number = foldl (\acc x -> 256*acc + toInteger x) 0
+-- Craft an arbitrary sized integer from a byte string
+encode_number = foldr (\x acc -> 256*acc + toInteger x) 0
+
+-- Reverse the procedure to get a string from an integer
+decode_number :: Integer -> [Word8]
+decode_number 0 = []
+decode_number n = (fromIntegral (mod n 256)) : decode_number (div n 256)
 
 oddify x = if x `mod` 2 == 1 then x else x+1
 
 byte_str_to_odd :: [Word8] -> Int -> Integer
-byte_str_to_odd str num_bytes = oddify (make_number (take num_bytes str))
+byte_str_to_odd str num_bytes = oddify (encode_number (take num_bytes str))
