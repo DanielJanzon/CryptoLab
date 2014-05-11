3c56157a8c40e159f9ce68cb0fbbdcfc1b8abf7a
Daniel Janzon <daniel.janzon@edgeware.tv>
2014-05-11 11:39:56 +0200

    This commit adds parallelism to the prime search in GeneratePrime.hs. A separate search is dispatched for each thread and there is no thread communication.
    The usage, e.g. to generate a 2048 bit prime on 4 CPU cores.
    
        ~$ ./GeneratePrime pprime 2048 4 +RTS -N4
    
    I haven't performed a statistical analysis but obviously it will
    not be four times as fast with four CPU cores. But the variance
    will decrease and you'll be lucky more often :)

--- GeneratePrime.hs
+++ GeneratePrime.hs
@@ -1,6 +1,7 @@
 module Main where
 
 import System.Environment
+import Control.Concurrent
 import Control.Exception
 import qualified Data.Maybe as Maybe
 import qualified Data.ByteString.Lazy as BSL
@@ -11,6 +12,7 @@ import qualified Random as Rnd
 dispatch :: [(String, [String] ->IO())]
 dispatch = [ ("pqprime", pqprime),
              ("prime", prime),
+             ("pprime", pprime),
              ("nbitint", nbitint),
              ("int", genint) ]
 
@@ -21,12 +23,21 @@ start = do
 
 print_usage :: IOError -> IO()
 print_usage err = do
-    putStrLn "\tnbitint n\t- generate a random n-bit integer"
-    putStrLn "\tint n    \t- generate a random integer less than n"
-    putStrLn "\tprime n  \t- generate a random n-bit prime"
-    putStrLn "\tpqprime n\t- generate two random primes p, q such that p is n-bit and p=2q+1"
+    putStrLn "\tnbitint n \t- generate a random n-bit integer"
+    putStrLn "\tint n     \t- generate a random integer less than n"
+    putStrLn "\tprime n   \t- generate a random n-bit prime"
+    putStrLn "\tpprime n m\t- generate a random n-bit prime with m parallel search threads,"
+    putStrLn "\t              must be run with +RTS -N<m>"
+    putStrLn "\tpqprime n \t- generate two random primes p, q such that p is n-bit and p=2q+1"
 
 main = start `catch` print_usage
+
+display_maybe_result :: Maybe Integer -> IO()
+display_maybe_result result = do
+    if result == Nothing then
+        putStrLn "search failed, try again"
+    else
+        putStrLn (show (Maybe.fromJust result))
 
 pqprime :: [String] -> IO()
 pqprime [num_bits] = do
@@ -38,10 +49,7 @@ prime :: [String] -> IO()
 prime [num_bits] = do
     start <- Rnd.random_int $ 2^(read num_bits)
     let p = NT.next_nbit_prime start (read num_bits)
-    if p == Nothing then
-        putStrLn "search failed, try again"
-    else
-        putStrLn (show (Maybe.fromJust p))
+    display_maybe_result p
 
 nbitint :: [String] -> IO()
 nbitint [num_bits] = do
@@ -54,3 +62,30 @@ genint [n] = do
     putStrLn $ show x
 
 
+{-
+   A parallel version of the prime search and its auxillary
+   functions. The idea is to let loose several threads that
+   all write to the same MVar if/when they are finished.
+   The main thread blocks on reading the MVar and when there
+   is anything to read it cancels the other threads (and
+   the finished thread too which is no harm done).
+-}
+cancel threadId = throwTo threadId ThreadKilled
+
+race_for_prime m bits startValue = forkIO $ do
+    let r = NT.next_nbit_prime startValue bits
+    putMVar m $! r -- Forcing evaluation with $! is crucial
+
+pprime :: [String] -> IO()
+pprime [num_bits_str, num_threads_str] = do
+    let num_bits = read num_bits_str
+    let num_threads = read num_threads_str
+    start <- mapM Rnd.random_int $ take num_threads $ repeat (2^num_bits)
+    m <- newEmptyMVar
+
+    threadIds <- mapM (race_for_prime m num_bits) start 
+
+    winner <- takeMVar m
+    mapM cancel threadIds
+
+    display_maybe_result winner
